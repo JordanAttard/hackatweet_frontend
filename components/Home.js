96f091abbf2fe400eb46ee29e87d165b489908b9
Florian DCL <docruzeiro.f@gmail.com>
2026-02-13 09:06:50 +0000

    ajout du dossier fonts dans public

--- components/Home.js
+++ components/Home.js
@@ -20,7 +20,9 @@ function Login() {
             alt="Logo"
             className={styles.smallLogo}
           />
-          <h1 className={styles.title}>See what´s happening</h1>
+          <h1 className={styles.title}>
+            See what's <br /> happening
+          </h1>
           <h2 className={styles.subtitle}>Join Hackatweet today.</h2>
 
           <button
